a5b3b158ee8f24b1c05eaf5bf232318f5f24a84e
Marise St-Pierre <naranjaqc@hotmail.com>
2023-02-16 15:59:03 -0500

    Ajoutez des fichiers projet.

---
+++ AbbyWeb/Model/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AbbyWeb.Model {
+    public class Category {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string NAme { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100)]
+        public int DisplayOrder { get; set; }
+    }
+}
