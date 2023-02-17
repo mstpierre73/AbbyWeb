0a3780bcae712d991c7cdc38bf459ef4defbf08a
Marise St-Pierre <naranjaqc@hotmail.com>
2023-02-17 09:34:06 -0500

    Connect DB, Add migrations, create index page

--- AbbyWeb/Data/ApplicationDbContext.cs
+++ AbbyWeb/Data/ApplicationDbContext.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AbbyWeb.Data {
     public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+            
+        }
         public DbSet<Category> Categories { get; set; }
     }
 }
