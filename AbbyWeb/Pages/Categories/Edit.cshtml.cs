53d5cd2b4201426211f6cb1f911868cc604bed50
Marise St-Pierre <naranjaqc@hotmail.com>
2023-02-21 09:23:50 -0500

    Add Edit page

---
+++ AbbyWeb/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,35 @@
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel { 
+
+        private readonly ApplicationDbContext _db;
+        
+        public Category Category { get; set; }
+
+        public EditModel(ApplicationDbContext db) {
+            _db = db;
+        }
+
+        public void OnGet(int id)
+        {
+            Category = _db.Category.Find(id);
+        }
+        public async Task<IActionResult> OnPost() {
+            if(Category.Name == Category.DisplayOrder.ToString()) {
+                ModelState.AddModelError("isNameEqualToDisplayOrder", "The Display Order can not exactly match the Name");
+            }
+            if (ModelState.IsValid) {
+                _db.Category.Update(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
